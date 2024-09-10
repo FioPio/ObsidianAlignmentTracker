a8df59940d1ad460d3d144b8d96dd9f538dce14a
Ferriol Pey Comas <ferriol73pey@gmail.com>
2024-09-10 14:31:12 +0200

    feat(api): add value scaling Implemented an autoscale of the values, now the user do not have to scale them and may just increase the numbers indefinitelly.

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-import { App, Plugin, PluginSettingTab, Setting, MarkdownRenderer } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
-export default class AlignmentTrackerPlugin extends Plugin {
-    async onload() {
+export default class AlignmentTrackerPlugin extends Plugin
+{
+    async onload()
+    {
         // Registers the block "alignmenttracker"
         this.registerMarkdownCodeBlockProcessor("alignmenttracker", (source, el, ctx) => {
             const values = this.parseSource(source);
-            this.renderAlignmentGrid(values, el);
+            const final_values = this.calculateValues(values);
+            this.renderAlignmentGrid(final_values, el);
         });
     }
 
     // Parses the content of the code block
-    parseSource(source: string) {
+    parseSource(source: string)
+    {
         const values = {
-            L: 0, N: 0, C: 0,
-            G: 0, N2: 0, E: 0,
+            L: 0, C: 0,
+            G: 0, E: 0,
         };
         source.split("\n").forEach(line => {
             const [key, value] = line.split(":");
@@ -24,21 +28,58 @@ export default class AlignmentTrackerPlugin extends Plugin {
         return values;
     }
 
+    // Calculate Alignment Values
+    calculateValues(values: any)
+    {
+      const final_values = {
+          LG: 0, LN: 0, LE: 0,
+          NG: 0, N:  0, NE: 0,
+          CG: 0, CN: 0, CE: 0,
+      };
+
+      // Get total variables
+      let total = values.L + values.C + values.G + values.E;
+      let N_LC = Math.floor((values.L + values.C)/2);
+      let N_GE = Math.floor((values.G + values.E)/2);
+
+      // Get the multiplying factor
+      let scale = 10.0 / total;
+
+      // Generate all the values:
+      /// Lawfull column
+      final_values.LG = Math.floor((values.L + values.G) * scale);
+      final_values.LN = Math.floor((values.L + N_GE)     * scale);
+      final_values.LE = Math.floor((values.L + values.E) * scale);
+
+      /// Neutral column
+      final_values.NG = Math.floor((N_LC + values.G) * scale);
+      final_values.N  = Math.floor((N_LC + N_GE)     * scale);
+      final_values.NE = Math.floor((N_LC + values.E) * scale);
+
+      /// Chaotic column
+      final_values.CG = Math.floor((values.C + values.G) * scale);
+      final_values.CN = Math.floor((values.C + N_GE)     * scale);
+      final_values.CE = Math.floor((values.C + values.E) * scale);
+
+      return final_values;
+    }
+
     // Renderizes the grid using HTML
-    renderAlignmentGrid(values: any, el: HTMLElement) {
+    renderAlignmentGrid(values: any, el: HTMLElement)
+    {
         const grid = document.createElement("div");
         grid.className = "alignment-grid";
 
         const alignments = [
-            { name: "LG", value: values.L },
-            { name: "NG", value: values.N },
-            { name: "CG", value: values.C },
-            { name: "LN", value: values.G },
-            { name: "N", value: values.N2 },
-            { name: "CN", value: values.E },
-            { name: "LE", value: values.L },
-            { name: "NE", value: values.N2 },
-            { name: "CE", value: values.E }
+            { name: "LG", value: values.LG },
+            { name: "NG", value: values.NG },
+            { name: "CG", value: values.CG },
+            { name: "LN", value: values.LN },
+            { name: "N",  value: values.N  },
+            { name: "CN", value: values.CN },
+            { name: "LE", value: values.LE },
+            { name: "NE", value: values.NE },
+            { name: "CE", value: values.CE }
         ];
 
         alignments.forEach(align => {
